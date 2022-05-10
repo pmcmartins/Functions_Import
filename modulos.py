d32ad0c44adef7aa5eb020e28631b4ae66178d9e
pmcmartins <45401064+pmcmartins@users.noreply.github.com>
2022-05-10 17:21:56 +0100

    Initial commit

---
+++ modulos.py
@@ -0,0 +1,14 @@
+"""from random import randint
+# call randint function to get random number
+
+help(randint)
+print(randint(10, 20)) """
+
+from  math import sqrt
+
+# use math module functions
+help(sqrt)
+print(sqrt(5))
+# Output 2.23606797749979
+
+
