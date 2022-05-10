92d7d8019561fccc38927582d6407c38f1006eda
pmcmartins <45401064+pmcmartins@users.noreply.github.com>
2022-05-10 19:51:48 +0100

    nn

--- lambda.py
+++ lambda.py
@@ -1,2 +1,15 @@
-ola = lambda: print('Olá mundo')
-ola() # -> Olá mundo
+'''numeros = [0,2,3,4,5,7]
+pares = []
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+# Exibe os números
+for par in pares:
+    print(par) '''
+
+numeros = [0,2,3,4,5,7]
+pares = filter(lambda valor: valor % 2 == 0, numeros)
+
+for par in pares:
+    print(par)
