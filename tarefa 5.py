cab377a24c9b6ca84ce9a8a3e7f03b08bc64fe23
pmcmartins <45401064+pmcmartins@users.noreply.github.com>
2022-05-12 12:43:00 +0100

    nn

--- tarefa 5.py
+++ tarefa 5.py
@@ -1,7 +1,18 @@
-list1 = [1,2,3,5,7,8]
-list2 = [2,3,5,6,7,8]
-
-result = list(map(lambda x: x == x in list1,list2))
-print(result)
-
-help(map)
+pedra = 0
+papel = 1
+tesoura = 2
+jogar_novamente = "Sim"
+    while (jogar_novamente == "Sim"):
+jogador1 = int(input("Jogador1, digite 0 p/pedra, 1 p/papel ou 2/tesoura: " ))
+jogador2 = int(input("Jogador2, digite 0 p/pedra, 1 p/papel ou 2/tesoura: " ))
+if (0 <= jogador1 <= 2) and (0 <= jogador2 <= 2):
+    if (jogador1 == jogador2):
+ print("Empate! Ninguém ganhou." ) # empate
+e   lif (jogador1 - jogador2) % 3 == 1:
+ print("Jogador 1 ganhou." )
+else:
+ print("Jogador 2 ganhou." )
+else:
+print("Opção inválida." )
+jogar_novamente = input("Você quer tentar novamente? Digite Sim ou Não" )
+print("Até a próxima!" )
--- teste.py
+++ teste.py
@@ -1,4 +1,9 @@
-def add(a, b):
-    return a + 5, b + 5
-result = add(3, 2)
-print(result)
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print(max(3, 5))
+print(max(5, 3))
+print(max(int(input()), int(input())))
